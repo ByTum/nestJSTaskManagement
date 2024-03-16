c09b8cc1a97e6d2e080c20eae734f18bebf2f353
Tum <tum-ok@msn.com>
2024-03-16 11:34:06 +0700

    Revert "Feature: Creating a Task (Part 1 - Controller)"
    This reverts commit 20c59b6115e52c318398e34843bf28deb4c8431e.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,13 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
+  getAllTasks() {
     return this.tasksService.getAllTasks();
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
 
 @Injectable()
 export class TasksService {
-  private tasks: Task[] = [];
+  private tasks = [];
 
-  getAllTasks(): Task[] {
+  getAllTasks() {
     return this.tasks;
   }
 }
